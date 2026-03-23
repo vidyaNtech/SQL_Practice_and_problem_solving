009033ae35df84fef200d3689005dd638a5fad32
Vidya N <vidyavidya12543@gmail.com>
2026-03-23 19:19:25 +0530

    Added Select All problem (Day 01)

---
+++ day_01/select_all.sql
@@ -0,0 +1,12 @@
+-- Problem: Select All
+-- Link: https://www.hackerrank.com/challenges/select-all-sql/problem
+
+-- Approach:
+-- 1. Retrieve all columns from CITY table
+-- 2. Use SELECT *
+
+SELECT *
+FROM CITY;
+
+-- Key Learning:
+-- SELECT * is used to fetch all columns
