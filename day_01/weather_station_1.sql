e455b98c98817439f9a073e562d44ec8a394de42
Vidya N <vidyavidya12543@gmail.com>
2026-03-23 21:01:07 +0530

    Update weather_station_1.sql

---
+++ day_01/weather_station_1.sql
@@ -0,0 +1,11 @@
+-- Problem: Weather Observation Station 1
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+-- Approach:
+-- 1. Select CITY and STATE
+
+SELECT CITY, STATE
+FROM STATION;
+
+-- Key Learning:
+-- Selecting multiple columns
