c0064c549e9f924e2ed35da16695cc9211bb7311
Vidya N <vidyavidya12543@gmail.com>
2026-03-23 19:24:19 +0530

    Added Select By ID SQL problem

---
+++ day_01/day_01/select_by_id.sql
@@ -0,0 +1,13 @@
+-- Problem: Select By ID
+-- Link: https://www.hackerrank.com/challenges/select-by-id/problem
+
+-- Approach:
+-- 1. Filter row using ID
+-- 2. Use WHERE condition
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
+
+-- Key Learning:
+-- WHERE is used to filter rows
