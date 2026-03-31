8b9deebbf0dbf51096df840be40f697684054a23
Vidya N <vidyavidya12543@gmail.com>
2026-03-31 20:06:44 +0530

    Day 10: Reinforced MIN with subquery and formatting (Weather Station 15)

---
+++ day_10/weather_station_15.sql
@@ -0,0 +1,19 @@
+-- Problem: Weather Observation Station 15
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-15/problem
+
+-- Approach:
+-- 1. Find smallest LAT_N greater than 38.7780
+-- 2. Use MIN with condition
+-- 3. Fetch corresponding LONG_W
+-- 4. Round result to 4 decimal places
+
+SELECT DECIMAL(ROUND(LONG_W, 4), 10, 4)
+FROM STATION
+WHERE LAT_N = (
+    SELECT MIN(LAT_N)
+    FROM STATION
+    WHERE LAT_N > 38.7780
+);
+
+-- Key Learning:
+-- Using MIN with subquery to fetch corresponding values and handling precision formatting
